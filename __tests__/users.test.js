e699e0371c9443d0fe06855c5abe768ac46493e1
Eduardo Duarte <dudu_duarte@hotmail.com>
2020-05-27 17:51:57 -0300

    feat(profile): criar model e acesso para o profile.

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -8,23 +8,24 @@ const config = require('config');
 const MongoClient = require('mongodb');
 const mongoURI = config.get('mongoURI')
 
+let connection;
+let db;
+
+beforeAll(async () => {
+  connection = await MongoClient.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+  db = await connection.db();
+  await db.collection('users').deleteMany({});
+});
+
+afterAll(async () => {
+  await connection.close();
+});
+
 describe('insert', () => {
-  let connection;
-  let db;
-
-  beforeAll(async () => {
-    connection = await MongoClient.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    db = await connection.db();
-    await db.collection('users').deleteMany({});
-  });
-
-  afterAll(async () => {
-    await connection.close();
-    await db.close();
-  });
+  
   it("It should response the GET method", async (done) => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
@@ -43,6 +44,13 @@ describe('insert', () => {
     .send({name: "Pedro", email: "pedro@hotmail.com", password: "123456"});
     const response1 = await request(app).post('/api/users')
     .send({name: "Pedro", email: "pedro@hotmail.com", password: "123456"});
+    expect(response1.statusCode).toBe(400);
+    done();
+  });
+  
+  it("should return status 400 because dont have profile", async (done) => {
+    const response = await request(app).post("/api/auth").send({ email: "eduardo@hotmail.com", password: "123456" });
+    const response1 = await request(app).get("/api/profile/me").set({'x-auth-token': response.body.token})
     expect(response1.statusCode).toBe(400);
     done();
   });
