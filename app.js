389db775a7fdeef8255e394796879dd9532be0aa
Eduardo Duarte <dudu_duarte@hotmail.com>
2020-05-30 16:08:49 -0300

    Prepare deploy

--- app.js
+++ app.js
@@ -7,11 +7,20 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
+
+
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('cleint/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 module.exports = app;
