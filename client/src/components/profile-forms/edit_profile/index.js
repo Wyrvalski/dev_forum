cd85382d1f2f92016de84f8e5eed678aa731faa8
Eduardo Duarte <dudu_duarte@hotmail.com>
2020-06-15 14:32:35 -0300

    feat(done) .

--- client/src/components/profile-forms/edit_profile/index.js
+++ client/src/components/profile-forms/edit_profile/index.js
@@ -51,14 +51,14 @@ const EditProfile = ({
       githubusername:
         loading || !profile.githubusername ? '' : profile.githubusername,
       bio: loading || !profile.bio ? '' : profile.bio,
-      twitter: loading || !profile.social.twitter ? '' : profile.social.twitter,
+      twitter: loading || !profile.social ? '' : profile.social.twitter,
       facebook:
-        loading || !profile.social.facebook ? '' : profile.social.facebook,
+        loading || !profile.social? '' : profile.social.facebook,
       linkedin:
-        loading || !profile.social.linkedin ? '' : profile.social.linkedin,
-      youtube: loading || !profile.social.youtube ? '' : profile.social.youtube,
+        loading || !profile.social ? '' : profile.social.linkedin,
+      youtube: loading || !profile.social ? '' : profile.social.youtube,
       instagram:
-        loading || !profile.social.instagram ? '' : profile.social.instagram
+        loading || !profile.social? '' : profile.social.instagram
     });
   }, [loading, getCurrentProfile]);
 
